b707b6ad12e131dc0fc789b9cebce4be5d0feb11
gonzagadavid <david.blesses@hotmail.com>
2022-12-07 23:26:25 -0300

    feat: add exercise 1.4

---
+++ chapter-1/exercise-1-4/celsius-to-fahrenheit.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+
+/*
+print Celsius-Fahrenheit table
+*/
+
+main() {
+  int celsius, fahr;
+  int lower, upper, step;
+
+  lower = 0; /* lower limit of temperature table */ 
+  upper = 300; /* upper limit */
+  step = 20; /* step size */
+
+  celsius = lower;
+
+  printf("Celsius\tFahrenheit\n");
+
+  while (celsius <= upper) {
+    fahr = celsius * 9 / 5 + 32;
+    printf("%d\t\t%d\n", celsius, fahr);
+    celsius = celsius + step;
+  }
+}
