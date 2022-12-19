66f426f1c0ad92f772e90b19c30c4b0361e73324
gonzagadavid <david.blesses@hotmail.com>
2022-12-18 21:11:14 -0300

    feat: add exercise 1-9

---
+++ chapter-1/exercise-1-9/replace-char.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// replace each string of one or more blanks by a single blank.
+
+main() {
+  int c, lastC;
+
+  while ((c = getchar()) != EOF) {
+    if(lastC == ' ' && c == ' ') {
+      continue;
+    }
+    lastC = c;
+    putchar(c); 
+  }
+}
