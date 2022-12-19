46918626ef1e5ef1465ccc6a27cd1d94000fbc1c
gonzagadavid <david.blesses@hotmail.com>
2022-12-18 21:42:31 -0300

    feat: add exercise 1-10

---
+++ chapter-1/exercise-1-10/replace-chars.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// replace tab by \t, each backspace by \b,  and each blackslash by \\.
+
+main() {
+  int c;
+
+  while ((c = getchar()) != EOF) {
+    if(c == '\t') {
+      putchar('\\');
+      putchar('t');
+      continue;
+    }
+    if(c == '\b') {
+      putchar('\\');
+      putchar('b');
+      continue;
+    }
+    if(c == '\\') {
+      putchar('\\');
+      putchar('\\');
+      continue;
+    }
+    putchar(c); 
+  }
+}
