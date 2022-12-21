f3c52bb8489c346dc6c30dc54ca58386d1ea9994
gonzagadavid <david.blesses@hotmail.com>
2022-12-21 15:56:00 -0300

    feat: add exercise 1-12

---
+++ chapter-1/exercice-1-12/one-word-per-line.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+# define IN 1 /* inside a word*/
+# define OUT 0 /* outside a word*/
+
+// /* print one word per line */
+
+main() {
+  int c;
+
+
+  while ((c = getchar()) != EOF) {
+    if (c == ' ' || c == '\n' || c == '\t') {
+      putchar('\n');
+   } else {
+      putchar(c);
+   } 
+  }
+}
