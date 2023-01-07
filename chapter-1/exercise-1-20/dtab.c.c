f90c939714b3fd048b1fa34362a809ca0b238644
gonzagadavid <david.blesses@hotmail.com>
2023-01-07 20:44:13 -0300

    feat: add exercise 1-20

--- chapter-1/exercise-1-20/dtab.c.c
+++ chapter-1/exercise-1-20/dtab.c.c
@@ -1,13 +1,17 @@
+
+
 #include <stdio.h>
 #define MAXLINE 1000 /* maximum input line size */
-#define MINIMUM_LINE 8
+
+int tabs_numbers = 8;
 
 int get_line(char line[], int maxline);
 void copy(char to[], char from[]);
-
+int dtab(char line[], int index);
 /* 
-write a program to print all input lines that are longer than
-80 characters
+write a program detab that replaces tabs in the input with the proper number 
+of blanks to space to the next tab stop. Assume a fixed set of tab stop,
+say every n columns. should n be a variable or a symbolic parameter? 
 */
 
 int main() {
@@ -19,10 +23,8 @@ int main() {
 
   count = 0;
   while ((len = get_line(line, MAXLINE)) > 0) {
-    if (len > MINIMUM_LINE) {
       copy(longer_lines[count], line);
       count++;
-    }
   } 
 
   if (count > 0) { /* there was a line */
@@ -38,7 +40,11 @@ int get_line(char s[], int lim) {
   int c, i;
 
   for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
-    s[i] = c;
+    if(c == ' ') {
+      i = dtab(s, i);
+    } else {
+      s[i] = c;
+    }
   }
   if (c == '\n') {
     s[i] = c;
@@ -55,4 +61,16 @@ void copy(char to[], char from[]) {
   while((to[i] = from[i]) != '\0') {
     ++i;
   }
+}
+
+
+int dtab(char line[], int index) {
+  int i;
+  extern int tabs_numbers;
+
+  for (i = index; i <= (index + tabs_numbers); ++i) {
+    line[i] = ' ';
+  }
+
+  return i; 
 }
